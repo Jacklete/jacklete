83face085c7ae7716ec33f33b529a574b2d4d3db
Harrie <harrie@zerofiltergrowth.com>
2025-11-16 22:29:27 +0100

    Update script

---
+++ script.js
@@ -0,0 +1,7 @@
+const button = document.getElementById("helloButton");
+
+if (button) {
+  button.addEventListener("click", () => {
+    alert("Dit is de eerste klik op de Jacklete site. De basis staat.");    
+  });
+}
